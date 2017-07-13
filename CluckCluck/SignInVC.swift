4566ceaa9e953355e070c03d7316360e2b5fa8ef
Tyron Spencer <tyronspencer@192-168-1-4.tpgi.com.au>
2017-07-13 20:28:25 +1000

    Completed setting up firebase integration

---
+++ CluckCluck/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CluckCluck
+//
+//  Created by Tyron Spencer on 13/7/17.
+//  Copyright © 2017 Tyron Spencer. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
